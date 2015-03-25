3df5923cf189994e1d3de01de1af01df2e13650e
Donovan Hoffman <donovanshaunhoffman@gmail.com>
2015-03-25 14:55:11 +1300

    Blocks are now in grid arrangement
    -styles for sketchpad added
    -unneccessary styles for block removed
    -Javascript now dynamically alters width and height of blocks to be a
    square grid

--- Javascript/blocky_sketch.js
+++ Javascript/blocky_sketch.js
@@ -1,13 +1,19 @@
 
 function createSketchPad(columns, rows) {
+  var $sketchpad = $('#sketchpad');
+  var height  = ($sketchpad.height() - (2 * rows) ) / rows;
+  var width   = ($sketchpad.width() - (2 * columns) ) / columns;
+
   for (currentRow = 0; currentRow < rows; currentRow++) {
     for(currentColumn = 0; currentColumn < columns; currentColumn++) {
-      createDiv();
+      createDiv(height, width);
     }
   }
 }
 
-function createDiv() {
+function createDiv(block_height, block_width) {
   var $block = $('<div class=\'block\'></div>');
+  $block.width(block_width);
+  $block.height(block_height)
   $block.appendTo($('#sketchpad'))
 }
