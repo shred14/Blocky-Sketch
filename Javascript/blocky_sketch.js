7085971dfc8f1d616ed632e32f360ccdf853516b
Donovan Hoffman <donovanshaunhoffman@gmail.com>
2015-03-25 15:59:19 +1300

    dammit forgot to include this

--- Javascript/blocky_sketch.js
+++ Javascript/blocky_sketch.js
@@ -1,19 +1,40 @@
+function createSketchPad() {
+  destroyBlocks();
+  var squares_per_side = prompt("How many squares per side would you like?");
 
-function createSketchPad(columns, rows) {
+  $button = $('button');
+  
+  if($button.text() === 'Build') {
+    $button.text('Build Again');
+  }
+
+  constructSketchPad(squares_per_side);
+}
+
+function constructSketchPad(squares_per_side) {
   var $sketchpad = $('#sketchpad');
-  var height  = ($sketchpad.height() - (2 * rows) ) / rows;
-  var width   = ($sketchpad.width() - (2 * columns) ) / columns;
+  var height  = ($sketchpad.height() - (2 * squares_per_side) ) / squares_per_side;
+  var width   = ($sketchpad.width() - (2 * squares_per_side) ) / squares_per_side;
 
-  for (currentRow = 0; currentRow < rows; currentRow++) {
-    for(currentColumn = 0; currentColumn < columns; currentColumn++) {
-      createDiv(height, width);
-    }
+  var number_of_blocks = squares_per_side * squares_per_side;
+  for (currentRow = 0; currentRow < number_of_blocks; currentRow++) {
+    createDiv(height, width);
   }
 }
 
 function createDiv(block_height, block_width) {
-  var $block = $('<div class=\'block\'></div>');
+  var $block = $('<div></div>');
+  $block.addClass('block');
   $block.width(block_width);
-  $block.height(block_height)
+  $block.height(block_height);
+  
+  $block.mouseenter(function() {
+      $(this).toggleClass('sketched');
+  });
+
   $block.appendTo($('#sketchpad'))
+}
+
+function destroyBlocks() {
+  $('.block').remove();
 }
